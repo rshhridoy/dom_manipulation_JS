b8caf181ff700549660ec4a6fd5469a703d73b3a
Hridoy <rshhridoy@gmail.com>
2026-09-14 00:29:56 +0600

    manipulate HTML and CSS with js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-let box = document.querySelectorAll(".box");
-console.dir(box)
+// let box = document.querySelectorAll(".box");
+// console.dir(box)
 
-// box[0].innerText = "new value 1";
-// box[1].innerText = "new value 2";
-// box[2].innerText = "new value 3";
+// // box[0].innerText = "new value 1";
+// // box[1].innerText = "new value 2";
+// // box[2].innerText = "new value 3";
 
 
-idx = 1;
+// idx = 1;
 
-for(boxs of box){
-    boxs.innerText = `New Value is : ${idx}`;
-    idx++;
-}
+// for(boxs of box){
+//     boxs.innerText = `New Value is : ${idx}`;
+//     idx++;
+// }
+
+
+let div = document.querySelector("div");
+console.dir(div);
+
+div.style.backgroundColor = "blue"; 
+div.style.backgroundColor = "purple"; 
+
+div.style.fontSize = "40px";
+
+div.innerText = "Hello!"
+
+// div.style.visibility = "hidden";

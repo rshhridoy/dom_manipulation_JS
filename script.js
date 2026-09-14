ae26561cd15a3468c5ff1700c72b1baf6e175347
Hridoy <rshhridoy@gmail.com>
2026-09-14 16:20:56 +0600

    toggle btn

--- script.js
+++ script.js
@@ -12,3 +12,37 @@ btn.onclick = () => {
 }
 
 document.querySelector("div").onmouseover = () => console.log("You are inside div");
+
+
+
+let clicked = document.querySelector("#clicked");
+
+
+//toggle button light to dark and dark to light
+
+let toggleBtn = document.querySelector("#toggle");
+let body = document.querySelector("body");
+let mode = "light";
+
+
+toggleBtn.addEventListener ("click", () =>  {
+    if(mode =="light"){
+        body.style.backgroundColor = "#121212";
+        toggleBtn.innerText = "Dark mode";
+        toggleBtn.style.color = "white";
+        toggleBtn.style.backgroundColor = "#121212";
+        toggleBtn.style.border = "1px solid white";
+        mode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }else{
+        body.style.backgroundColor = "white";
+        toggleBtn.innerText = "Light mode";
+        toggleBtn.style.color = "#121212";
+        toggleBtn.style.backgroundColor = "white";
+        toggleBtn.style.border = "1px solid #121212";
+        mode = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+})
